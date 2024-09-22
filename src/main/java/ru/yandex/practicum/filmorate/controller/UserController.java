523f52d28e0a34d230b38ad87411142d026dda3f
Артём Уханов <gartemonka@mail.ru>
2024-09-22 22:37:07 +0300

    Фз 11 спринта

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,57 +1,61 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
-import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
 
 import java.util.Collection;
-import java.util.HashMap;
 
-@Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
-    private HashMap<Long, User> users = new HashMap<>();
+    private final UserService userService;
 
     @GetMapping
     public Collection<User> findAll() {
-        return users.values();
+        return userService.findAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.getUser(id);
+    }
+
+    @GetMapping("/{id}/friends")
+    public Collection<User> getListOfFriends(@PathVariable Long id) {
+        return userService.getListOfFriends(id);
+    }
+
+    @GetMapping("/{id}/friends/common/{otherId}")
+    public Collection<User> getMutualFriends(@PathVariable Long otherId, @PathVariable Long id) {
+        return userService.getListOfMutualFriends(id, otherId);
     }
 
     @PostMapping
     public User create(@Valid @RequestBody User user) {
-        validateUserName(user);
-        user.setId(getNextId());
-        users.put(user.getId(),user);
-        log.info("User {} created " + user.getId());
-        return user;
+        return userService.createUser(user);
     }
 
     @PutMapping
     public User update(@Valid @RequestBody User newUser) {
-        if (!users.containsKey(newUser.getId())) {
-            throw new ValidationException("Пользователя с таким id нет");
-        }
-        validateUserName(newUser);
-        users.put(newUser.getId(), newUser);
-        log.info("User {} updated " + newUser.getId());
-        return newUser;
+        return userService.updateUser(newUser);
     }
 
-    private void validateUserName(User user) {
-        if (user.getName() == null || user.getName().isEmpty()) {
-            user.setName(user.getLogin());
-        }
+    @PutMapping("/{id}/friends/{friendId}")
+    public User addFriend(@PathVariable Long friendId, @PathVariable Long id) {
+        return userService.addFriend(id, friendId);
     }
 
-    private long getNextId() {
-        long currentMaxId = users.keySet()
-                .stream()
-                .mapToLong(id -> id)
-                .max()
-                .orElse(0);
-        return ++currentMaxId;
+    @DeleteMapping("/{id}")
+    public User deleteUser(@PathVariable Long id) {
+        return userService.deleteUser(id);
+    }
+
+    @DeleteMapping("/{id}/friends/{friendId}")
+    public User removeUserFromFriends(@PathVariable Long friendId, @PathVariable Long id) {
+        return userService.deleteFriend(id, friendId);
     }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -0,0 +1,89 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserStorage userStorage;
+
+    public Collection<User> findAllUsers() {
+        return userStorage.findAll();
+    }
+
+    public User getUser(Long id) {
+        return userStorage.getUser(id);
+    }
+
+    public User deleteUser(Long id) {
+        return userStorage.deleteUser(id);
+    }
+
+    public User createUser(User user) {
+        return userStorage.create(user);
+    }
+
+    public User updateUser(User user) {
+        return userStorage.update(user);
+    }
+
+    public User addFriend(Long userId, Long friendId) {
+        User user = userStorage.getUser(userId);
+        User friend = userStorage.getUser(friendId);
+        checkValidUser(user);
+        checkValidUser(friend);
+        user.getFriends().add(friendId);
+        friend.getFriends().add(userId);
+        userStorage.update(user);
+        userStorage.update(friend);
+        log.info("User {} add friend {}", userId, friendId);
+        return user;
+    }
+
+    public User deleteFriend(Long userId, Long friendId) {
+        User user = userStorage.getUser(userId);
+        User friend = userStorage.getUser(friendId);
+        checkValidUser(user);
+        checkValidUser(friend);
+        user.getFriends().remove(friendId);
+        friend.getFriends().remove(userId);
+        userStorage.update(user);
+        userStorage.update(friend);
+        log.info("User {} delete friend {}", userId, friendId);
+        return user;
+    }
+
+    public Collection<User> getListOfFriends(Long userId) {
+        User user = userStorage.getUser(userId);
+        checkValidUser(user);
+        return user.getFriends().stream()
+                .map(userStorage::getUser)
+                .collect(Collectors.toList());
+    }
+
+    public Collection<User> getListOfMutualFriends(Long userId, Long friendId) {
+        User user = userStorage.getUser(userId);
+        User friend = userStorage.getUser(friendId);
+        checkValidUser(user);
+        checkValidUser(friend);
+        return user.getFriends().stream()
+                .filter(id -> friend.getFriends().contains(id))
+                .map(userStorage::getUser)
+                .collect(Collectors.toList());
+    }
+
+    private void checkValidUser(User user) {
+        if (user == null) {
+            throw new NotFoundException("Юзер не может быть null");
+        }
+    }
+}
