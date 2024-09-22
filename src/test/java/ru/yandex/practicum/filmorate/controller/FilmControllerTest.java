8c8606d90381d15e33820d5e04eaab1328a9843e
Артём Уханов <gartemonka@mail.ru>
2024-09-22 23:10:36 +0300

    remove unused imports in tests

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -4,10 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.yandex.practicum.filmorate.FilmorateApplication;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.time.LocalDate;
